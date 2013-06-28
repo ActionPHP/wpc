2e637898eb0caece41ae6610c4a0a62479eddc1e
Jean <jean@jean-MS-7529.(none)>
2013-06-28 19:20:24 +0100

    'Regular update'

--- includes/class-product.php
+++ includes/class-product.php
@@ -1,5 +1,98 @@
 <?php
 class WPCartProduct
 {
+	protected $id;
+	protected $category_id;
+	protected $name;
+	protected $brief_description;
+	protected $description;
+	protected $tags;
+	protected $properties;
+
+	public function create($data)
+	{
+		# code...
+		$id = 1; //This will be replaced by id returned from db
+
+		$this->setId($id);
+
+		$data->id = $this->getId();
+		return $data;
+	}
+
+	public function get($id)
+	{
+		# code...
+	}
+
+	public function update($data)
+	{
+		# code...
+	}
+
+	public function remove($id)
+	{
+		# code...
+	}
+
+	public function setId($id)
+	{
+		$this->id = $id;
+	}
+
+	public function setName($name)
+	{
+		$this->name = $name ;
+	}
+
+	public function setCategoryId($category_id)
+	{
+		$this->category_id = $category_id ;
+	}
+
+	public function setBriefDescription($brief_description)
+	{
+		$this->brief_description = $brief_description;
+	}
+
+	public function setDescription($description)
+	{
+		$this->description = $description;
+	}
+
+	public function setTags()
+	{
+		$this->tags = $tags;
+	}
+
+	public function getId()
+	{
+		return $this->id;
+	}
+
+	public function getName()
+	{
+		# code...
+	}
+
+	public function getCategoryId()
+	{
+		# code...
+	}
+
+	public function getBriefDescription()
+	{
+		# code...
+	}
+
+	public function getDescription()
+	{
+		# code...
+	}
+
+	public function getTags()
+	{
+		# code...
+	}
 }
 ?>
--- tests/test-class-product.php
+++ tests/test-class-product.php
@@ -4,5 +4,80 @@ require_once('../includes/class-product.php');
  
 class TestWPCartProduct extends UnitTestCase 
 {
+	public function __construct()
+	{
+			$this->product = new WPCartProduct;
+	}
+	public function testInit()
+	{
+		$this->assertIsA($this->product, 'WPCartProduct');
+	}
+
+	public function testSettersGetters()
+	{
+
+	}
+
+	public function testCreate()
+	{	
+		$product = $this->product;
+		$data = array(
+
+			'family' => 'GigTicket',
+			'category_id' => 5,
+			'name' => 'Easter Gig',
+			'price' => '97.00',
+			'brief_description' => 'The Gigger\'s Gig',
+			'description' => 'This is the gig that will blow your mind. You have never been to a gig like this.',
+			'tags' => array('gig', 'easter', 'live show', 'paid gig' ),
+			'properties' => (object) array( '1' =>  array('venue' => 'St Albans Square'), '12' =>  array('band' => 'Aeden Rain'), '5' => array('start_time' => '10:30AM'), '7' => array('date' => '25th December, 2013')),
+
+		);
+
+		$data = (object) $data;
+		
+		print_r($data);
+
+		$product->create($data);
+		$product_id = $product->getId();
+
+		$this->assertNotNull($product_id); //Once the product has been created, the object needs to include the id. It cannot be null.
+		$this->assertIsA($product_id, 'int'); //The id must be an integer
+		$positive = ($product_id > -1);
+		$this->assertTrue($positive);
+
+	}
+
+
+	public function testGet()
+	{
+		$product_id = 1;
+		$product = $this->product;
+
+		$product->get($product_id);
+
+
+
+
+
+
+	}
+	public function testUpdate()
+	{
+
+	}
+
+
+	public function testRemove()
+	{
+
+	}
+
+
+	public function testGetAll()
+	{
+
+
+	}
 }
 ?>
